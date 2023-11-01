79078607d1cd34b37546dc83e85cf5697e600880
starboy011 <rishavofficial294@gmail.com>
2023-11-01 23:02:28 +0530

    Made the endpoint

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+function App() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/getName',{
+      headers: {
+        'If-None-Match': 'no-match', // Unlikely value
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data:', data); // Add this line for debugging
+        setName(data.name);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
   return (
-    <div>
-      rishav
+    <div className="App">
+      <h1>Name: {name}</h1>
     </div>
-  )
+  );
 }
+export default App;
