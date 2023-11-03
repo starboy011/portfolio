f6505afbd0ed50e4b45025c5e6ae765e80c56a36
starboy011 <rishavofficial294@gmail.com>
2023-11-03 14:01:46 +0530

    navbar

--- frontend/portfolio/src/components/navbar/Navbar.jsx
+++ frontend/portfolio/src/components/navbar/Navbar.jsx
@@ -2,7 +2,18 @@ import "./navbar.scss"
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">Navbar
+    <div className="wrapper">
+        <span>Rishav Choudhary</span>
+        <div className="social">        
+        <a href="#"><img src="../navbar/images/facebook.jpg"/></a>
+        {/* <a href="#"><img src="../navbar/images/gfg.png"/></a>
+        <a href="#"><img src="../navbar/images/Githubpng.png"/></a>
+        <a href="#"><img src="../navbar/images/Instagram.png"/></a>
+        <a href="#"><img src="../navbar/images/linkedin.png"/></a> */}
+        </div>
+    </div>
+    </div>
   )
 }
 
