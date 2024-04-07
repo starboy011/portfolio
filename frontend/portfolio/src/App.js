ca3ba5bc6c6ccb5e411cf6722a4c76d34d560b80
Rishav Choudhary <122264789+starboy011@users.noreply.github.com>
2024-04-07 18:51:18 +0100

    Update App.js

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -2,7 +2,6 @@ import "./app.scss"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Parallax from "./components/parallax/Parallax"
-
 function App() {
   return (
     <div>
