0c51868eeab38f3ad37480518f15f9440c9832ce
starboy011 <rishavofficial294@gmail.com>
2024-05-01 10:41:39 +0100

    changed opacity

--- frontend/portfolio/src/components/section1/Section.js
+++ frontend/portfolio/src/components/section1/Section.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Section.css"; // Import CSS file
 
 const Section = () => {
+  useEffect(() => {
+    const sectionDivs = document.querySelectorAll(".section-div");
+
+    const handleScroll = () => {
+      sectionDivs.forEach((div, index) => {
+        const rect = div.getBoundingClientRect();
+        const isVisible = rect.top < window.innerHeight - 50;
+        if (isVisible) {
+          div.classList.add("animate");
+        } else {
+          div.classList.remove("animate");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    // Clean up
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <>
-      <div className="section-container">
-        <div className="section-div-container">
-          <div className="section-div"></div>
-          <div className="section-div"></div>
-          <div className="section-div"></div>
-        </div>
+    <div className="section-container">
+      <div className="section-div-container">
+        <div className="section-div"></div>
+        <div className="section-div"></div>
+        <div className="section-div"></div>
       </div>
-    </>
+    </div>
   );
 };
 
