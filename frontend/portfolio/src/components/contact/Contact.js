f18ea3001848b70f39a9115820d2d1febcfbb1dc
starboy011 <rishavofficial294@gmail.com>
2024-05-03 18:01:54 +0100

    email integrated

--- frontend/portfolio/src/components/contact/Contact.js
+++ frontend/portfolio/src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import "./Contact.css";
+import "./Contact.css"; // Importing CSS file if you need styling
+
 export const Contact = () => {
   const form = useRef();
 
@@ -22,57 +23,53 @@ export const Contact = () => {
   };
 
   return (
-    <div className="contactcontainer">
-      <div className="formcontainer">
-        <div className="contactimage">
-          <div className="contacttitle">
-            <p>I'd love to here from</p>
-            <p1>YOU!</p1>
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="contactcontainer">
+        <div className="formcontainer">
+          <div className="contactimage">
+            <div className="contacttitle">
+              <p>I'd love to here from</p>
+              <p1>YOU!</p1>
+            </div>
           </div>
-        </div>
-        <div className="form">
-          <div className="heading">
-            <div className="text">
-              <p>Contact ME!</p>
+          <div className="form">
+            <div className="heading">
+              <div className="text">
+                <p>Contact ME!</p>
+              </div>
+              <div className="effect"></div>
             </div>
-            <div className="effect"></div>
-          </div>
-          <div className="mail">
-            <div className="contentcontainer">
-              <input
-                type="text"
-                name="user_name"
-                placeholder="Name"
-                className="name"
-              />
-            </div>
-            <div className="contentcontainer">
-              <input
-                type="email"
-                name="user_email"
-                placeholder="Email"
-                className="email"
-              />
-            </div>
-            <div className="contentcontainer">
-              <textarea name="message" className="message" />
-            </div>
-            <div className="sendcontainer">
-              <div className="beforesend"></div>
-              <p className="send">Send</p>
+            <div className="mail">
+              <div className="contentcontainer">
+                <input
+                  placeholder="Name"
+                  type="text"
+                  name="user_name"
+                  className="name"
+                />
+              </div>
+              <div className="contentcontainer">
+                <input
+                  placeholder="Email"
+                  type="email"
+                  name="user_email"
+                  className="email"
+                />
+              </div>
+              <div className="contentcontainer">
+                <textarea name="message" className="message" />
+              </div>
+              <div className="sendcontainer">
+                <div className="beforesend"></div>
+                <input type="submit" value="Send" className="send" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form> */}
-    </div>
+    </form>
   );
 };
+
+// .sendForm("service_ye0fs09", "template_i9qoaru", form.current, {
+//     publicKey: "DtuRo0tlW_Q5W7pk5",
