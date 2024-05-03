024e2b18e67f0b85b6750a1e2adc7d351bd4a8b9
starboy011 <rishavofficial294@gmail.com>
2024-05-03 13:26:36 +0100

    email functionality added

---
+++ frontend/portfolio/src/components/contact/Contact.js
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_ye0fs09", "template_i9qoaru", form.current, {
+        publicKey: "DtuRo0tlW_Q5W7pk5",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contactcontainer"></div>
+    // <form ref={form} onSubmit={sendEmail}>
+    //   <label>Name</label>
+    //   <input type="text" name="user_name" />
+    //   <label>Email</label>
+    //   <input type="email" name="user_email" />
+    //   <label>Message</label>
+    //   <textarea name="message" />
+    //   <input type="submit" value="Send" />
+    // </form>
+  );
+};
