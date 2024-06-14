c93de1b4dac7d598df46c9550247b379926060c1
Rishav Choudhary <122264789+starboy011@users.noreply.github.com>
2024-06-14 09:48:54 +0100

    Update App.js

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -6,7 +6,6 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Section from "./components/section1/Section";
 function App() {
-
   return (
     <div>
       <Navbar />
